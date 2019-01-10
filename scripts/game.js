aefc92ae3f8eac03b6e2508c2f4d6372dc0ea06e
Joanna <lewj@oregonstate.edu>
2019-01-10 12:48:31 -0800

    add collision between player and wall

--- scripts/game.js
+++ scripts/game.js
@@ -2,12 +2,14 @@ class Game {
     constructor(canvasId) {
         this.canvas = document.getElementById(canvasId);
         this.screen = this.canvas.getContext('2d');
-        this.gameSize = { x: this.canvas.clientWidth, y: this.canvas.height };
-        this.bodies = [new Player(this, this.gameSize)];
+        this.gameSize = { x: this.canvas.width, y: this.canvas.height };
+        this.bodies = [new Player(this, this.gameSize),
+                       new Wall(this, this.gameSize, 65, 35, 0, -50)];
 
         this.tick = this.tick.bind(this);
         this.update = this.update.bind(this);
         this.draw = this.draw.bind(this);
+        this.checkCollision = this.checkCollision.bind(this);
 
         this.tick();
     }
@@ -22,20 +24,29 @@ class Game {
         for (let i = 0; i < this.bodies.length; i++) {
             this.bodies[i].update();
         }
+
+        for (let i = 1; i < this.bodies.length; i++){
+            if (this.checkCollision(this.bodies[0], this.bodies[i])) {
+                this.bodies[0].center.x = this.bodies[0].oldCenter.x;
+                this.bodies[0].center.y = this.bodies[0].oldCenter.y;
+                this.bodies[i].center.x = this.bodies[i].oldCenter.x;
+                this.bodies[i].center.y = this.bodies[i].oldCenter.y;
+            }
+        }
     }
 
     draw() {
-        // var drawRect = function(screen, body) {
-        //     screen.fillRect(body.center.x - body.size.x / 2,
-        //                     body.center.y - body.size.y / 2,
-        //                     body.size.x, body.size.y);
-        // }
-
-        // this.screen.clearRect(0, 0, this.gameSize.x, this.gameSize.y);
-
         for (let i = 0; i < this.bodies.length; i++) {
             this.bodies[i].draw();
         }
+    }
+
+    checkCollision(body1, body2) {
+        return !(body1 === body2 || 
+                 body1.center.x + body1.size.x / 2 < body2.center.x - body2.size.x / 2 ||
+                 body1.center.y + body1.size.y / 2 < body2.center.y - body2.size.y / 2 || 
+                 body1.center.x - body1.size.x / 2 > body2.center.x + body2.size.x / 2 ||
+                 body1.center.y - body1.size.y / 2 > body2.center.y + body2.size.y / 2);
     }
 }
 
